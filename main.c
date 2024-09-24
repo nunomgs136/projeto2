af5bdb48e946dd578b559f0946836a7946943c49
Nuno <nunomargsilva@gmail.com>
2024-09-23 22:38:40 -0300

    Testes adicionais

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 
 int main(void) {
   printf("Hello World\n");
-  printf("Testando gitHub no replit");
+  printf("Testando gitHub no replit\n");
+  printf("testando git no vs code\n");
+  printf("Deu certo, manas\n");
+  printf("Testando branches no vs code");
+  printf("Tentando mais um pouco");
   return 0;
 }
