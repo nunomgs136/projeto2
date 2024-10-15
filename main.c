61afae8ce717cc16790e847e819500b523024625
Nuno <nunomargsilva@gmail.com>
2024-10-15 00:44:08 +0000

    sei la

--- main.c
+++ main.c
@@ -8,5 +8,7 @@ int main(void) {
   printf("Deu certo, manas\n");
   printf("Testando branches no vs code\n");
   printf("Tentando mais um pouco\n");
+  printf("Num sei\n");
+  printf("slay\n");
   return 0;
 }
